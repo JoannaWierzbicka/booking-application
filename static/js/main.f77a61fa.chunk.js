fa6b15029336edf3aed3f7bab5a671770cd6f0be
Joanna Wierzbicka <joannawierzbicka@poczta.onet.pl>
2022-11-15 13:45:50 +0100

    Updates

--- static/js/main.f77a61fa.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpbooking-application"]=this["webpackJsonpbooking-application"]||[]).push([[0],{10:function(n,o,t){"use strict";t.r(o);t(0);var i=t(3),e=t.n(i),c=t(1),p=function(){return Object(c.jsx)("div",{children:"Booking APP"})};t(9);e.a.render(Object(c.jsx)(p,{}),document.getElementById("root"))},9:function(n,o,t){}},[[10,1,2]]]);
-//# sourceMappingURL=main.f77a61fa.chunk.js.map
